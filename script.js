1bc73c37c9e0c9e4a1702ee34bb2e74395b7db11
Arman <armanulhaq10@gmail.com>
2024-02-10 20:37:27 +0530

    final-commit

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ var cardano = document.getElementById('cardano');
 var ripple = document.getElementById('ripple');
 
 var settings = {
-  async: true,
-  scrossDomain: true,
+  async: true, //This means the request is asynchronous. The script will continue to run while the request is being made in the background.
+  crossDomain: true,
   url: 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin%2Ctether%2Ccardano%2Cripple&vs_currencies=usd',
   method: 'GET',
   headers: {},
 };
 $.ajax(settings).done(function (response) {
+  //This method performs an Ajax (asynchronous HTTP) request.
   btc.innerHTML = response.bitcoin.usd;
   dgc.innerHTML = response.dogecoin.usd;
   eth.innerHTML = response.ethereum.usd;
